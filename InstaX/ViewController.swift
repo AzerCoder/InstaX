375ec912198b90d6ce8f113afb4975c61fcaa403
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2023-11-10 11:48:50 +0500

    Initial Commit

---
+++ InstaX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaX
+//
+//  Created by A'zamjon Abdumuxtorov on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
